b8ef5c051bff6d4ad6f9d20238ca79329a11be29
Mauricio Chaves <mauricio.chaves.jr@gmail.com>
2026-03-27 21:35:38 -0300

    fix(trade): add @Size(max=10) to OrderRequestDto.symbol to enforce DB constraint at DTO level

--- src/main/java/com/pilotbroker/web/dto/trade/OrderRequestDto.java
+++ src/main/java/com/pilotbroker/web/dto/trade/OrderRequestDto.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,7 @@ import lombok.NoArgsConstructor;
 public class OrderRequestDto {
 
     @NotBlank
+    @Size(max = 10)
     private String symbol;
 
     @NotBlank
